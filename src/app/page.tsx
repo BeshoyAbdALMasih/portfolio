2d6c502d47abda1bce78fc997c41ca2080dc5915
Beshoy Abd Al-Masih <bosha.abdo.4@gmail.com>
2025-08-24 17:09:23 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -59,17 +59,7 @@ export default function Page() {
               key={work.company}
               delay={BLUR_FADE_DELAY * 6 + id * 0.05}
             >
-              <ResumeCard
-                key={work.company}
-                logoUrl={work.logoUrl}
-                altText={work.company}
-                title={work.company}
-                subtitle={work.title}
-                href={work.href}
-                badges={work.badges}
-                period={`${work.start} - ${work.end ?? "Present"}`}
-                description={work.description}
-              />
+            
             </BlurFade>
           ))}
         </div>
